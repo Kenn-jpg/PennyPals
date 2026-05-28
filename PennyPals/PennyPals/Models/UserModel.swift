a12e5f3497ebff8134424e258bea3a4dbae90a8d
fenrir871 <wahyudibertrand02@gmail.com>
2026-05-28 12:46:19 +0700

    Creation of GoalModel,UserModel, and AuthViewModel
    Push Progreess:
    - AuthViewModel
    - GoalModel
    -UserModel

--- PennyPals/PennyPals/Models/UserModel.swift
+++ PennyPals/PennyPals/Models/UserModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct UserModel: Identifiable, Codable {
+    @DocumentID var id: String?
+    var username: String
+    var email: String
+    var coins: Int
+    var streak: Int
+    var lastLoginDate: Date
+    var createdAt: Date
+    
+    // Status penalti
+    var isSafeFromPenalty: Bool
+    var nextPenaltyCheck: Date
+}
