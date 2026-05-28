e30f09806614f74cbb66cf36b417d18306c2a878
Keane Juan <keanejuan07@gmail.com>
2026-05-28 10:08:31 +0700

    Add UserModel and AuthViewModel
    Add stub files UserModel.swift and AuthViewModel.swift containing file headers and Foundation imports. These are placeholder files for the User model and authentication view model (created 28/05/26) to be implemented later.

---
+++ PennyPals/PennyPals/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  PennyPals
+//
+//  Created by Keane Juan Suryanto on 28/05/26.
+//
+
+import Foundation
---
+++ PennyPals/PennyPals/ViewModels/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  PennyPals
+//
+//  Created by Keane Juan Suryanto on 28/05/26.
+//
+
+import Foundation
