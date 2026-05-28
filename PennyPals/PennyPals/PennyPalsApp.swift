ef62d96fde3548e12337ae9ae07fc23c7e28b8ed
Keane Juan <keanejuan07@gmail.com>
2026-05-28 08:00:24 +0700

    Initial commit: Setup PennyPals Xcode project with SwiftUI

---
+++ PennyPals/PennyPals/PennyPalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyPalsApp.swift
+//  PennyPals
+//
+//  Created by Keane Juan Suryanto on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PennyPalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
